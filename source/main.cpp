c4e35c7754f2b3926d76699926d47f60f98c891c
NIKIRIKI <sawwa.cwer13@gmail.com>
2024-08-13 12:58:20 +0300

    update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "include/dog.h"
+#include "include/oper.h"
 
 using std::cout, std::endl;
 
 int main()
 {
-    cout << "Hello world" << endl;
+    setlocale(LC_ALL, "Ru");
+    Dog dog1("James", 4);
+    dog1.bark();
     return 0;
 }
